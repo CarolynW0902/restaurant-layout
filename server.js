7a02bbf7c34d6e192afca98d977309230bb4a42b
Carolyn Wilson <carolynw0902@gmail.com>
2024-08-25 14:29:17 -0400

    edited the server.js

--- server.js
+++ server.js
@@ -23,6 +23,38 @@ db.once('open', () => {
   console.log("connected to MongoDB")
 })
 
+//Define a schema and model for the form data
+const contactSchema = new mongoose.Schema({
+  name: String,
+  people: Number,
+  date: Date,
+  message: String
+})
+
+const Contact = mongoose.model("Contact", contactSchema)
+
+//Handle form submission request
+app.post('/submit', async (req,res) => {
+  const formData = {
+    name: req.body.Name,
+    people: req.body.People,
+    date: new Date(req.body.Date),
+    message: req.body.Message
+  }
+
+  try{
+    const newContact = new Contact(formData)
+    await newContact.save()
+    res.redirect('/?success')
+  } catch (error){
+    res.redirect('/?error')
+  }
+})
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
 
 //start server
 const PORT = 4000
